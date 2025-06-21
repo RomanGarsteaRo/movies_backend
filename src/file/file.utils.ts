127fb4634269043c94df74f54cf6b3f81c4e0c3a
Roman Garstea <roman.garstea.ro@gmail.com>
2025-06-21 09:29:38 -0400

    Updating File Utils

--- src/file/file.utils.ts
+++ src/file/file.utils.ts
@@ -1,5 +1,8 @@
 import { IFile, IFileSize, ITitleParse, SizeUnits } from "./file.interface";
-
+import path from "path";
+import fs from "fs";
+import * as ffmpeg from 'fluent-ffmpeg';
+import { extname } from 'path';
 
 export class FileUtils{
 
@@ -47,6 +50,54 @@ export class FileUtils{
                 sizeInByte: bytes,
                 sizeInUnit: bytes / SizeUnits[unit],
                 unit: unit
+            };
+        }
+    }
+
+
+
+
+
+    static setIFileFromPath(fullPath: string, callback: (path: string) => IFile[]): IFile{
+        const stats = fs.statSync(fullPath);
+
+        if (stats.isDirectory()) {
+            // const entry = {
+            //     type: 'directory',
+            //
+            //     path: path.resolve(fullPath),
+            //     path_p: path.parse(fullPath),
+            //     titl_p: FileUtils.parseBaseName(path.basename(fullPath)),
+            //
+            //     size: FileUtils.convertBytesToIFileSize(0),
+            //     created: stats.birthtime.toString(),
+            //     modified: stats.mtime.toString(),
+            //     access: stats.atime.toString(),
+            // };
+            //
+            // // Obține structura recursivă a subdirectorului
+            // entry['children'] = callback(fullPath);
+            //
+            // // Calculăm dimensiunea totală a directorului
+            // entry.size = FileUtils.convertBytesToIFileSize(
+            //     entry['children']
+            //         .map((child) => child.size.sizeInByte || 0)     // Adună dimensiunile copiilor (în bytes)
+            //         .reduce((acc, size) => acc + size, 0)           // Dimensiunea totală în bytes
+            // );
+            //
+            // return entry;
+        } else {
+            return  {
+                type: 'file',
+
+                path: path.resolve(fullPath),
+                path_p: path.parse(fullPath),
+                titl_p: FileUtils.parseBaseName(path.basename(fullPath)),
+
+                size: FileUtils.convertBytesToIFileSize(stats.size), // Dimensiunea fișierului
+                created: stats.birthtime.toString(),
+                modified: stats.mtime.toString(),
+                access: stats.atime.toString(),
             };
         }
     }
